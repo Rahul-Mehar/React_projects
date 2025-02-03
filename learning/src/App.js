a9c3fdda3545ef010803ad0e0c206ac5a59aa706
Rahul-Mehar <rahulmehar92@gmail.com>
2025-02-03 12:52:48 +0530

    using multipell variable changes in a time

--- learning/src/App.js
+++ learning/src/App.js
@@ -3,16 +3,39 @@ import './App.css';
 
 function App() {
 
- const [color, setColor] = useState('_____')
+//  const [color, setColor] = useState('_____')
 
- const changecolor = () =>{
-  setColor("pink")
- }
+//  const changecolor = () =>{
+//   setColor("pink")
+//  }
+
+
+// const [brand, setBrand] = usestate("ferrari");
+// const [model, setModel] = usestate("Roma");
+// const [year, setYear] = usestate("2023");
+// const [color, setColor] = usestate("red");
+
+const [car,setCar] = useState({
+  brand:"Farrai",
+  model:"Rome",
+  year:"2023",
+  color:"Red"
+})
+
+
+const changecolor = ()=>{
+  setCar((prev)=>{
+    return {brand:"Audi", mode:"creta",color: "blue", year:"2024"}
+  })
+}
 
   return (
     <>
-    <h1>My favourite color is {color} </h1>
-    <button onClick={changecolor}>blue</button>
+    {/* <h1>My favourite color is {color} </h1>
+    <button onClick={changecolor}>blue</button> */}
+    <h1> My {car.brand}</h1>
+    <h2>It us a {car.color} {car.model} From {car.year} </h2>
+    <button onClick={changecolor}>Blue</button>
     </>
   );
 }
